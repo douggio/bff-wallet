e2ec70283ef00146671ad269e3c80d80d9f8cd5b
Douglas Giovanini <douglas_giovanini@hotmail.com>
2022-06-08 18:49:48 +0100

    initial commit

---
+++ src/main/java/com/douggio/bffwallet/BffWalletApplication.java
@@ -0,0 +1,13 @@
+package com.douggio.bffwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douggio/bffwallet/BffWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douggio.bffwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
