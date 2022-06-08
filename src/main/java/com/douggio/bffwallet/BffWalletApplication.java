929c8b9749b1dd55e064e09d9f94b57232b6e503
Douglas Giovanini <douglas_giovanini@hotmail.com>
2022-06-08 19:33:08 +0100

    setup eureka client

--- src/main/java/com/douggio/bffwallet/BffWalletApplication.java
+++ src/main/java/com/douggio/bffwallet/BffWalletApplication.java
@@ -2,8 +2,10 @@ package com.douggio.bffwallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BffWalletApplication {
 
 	public static void main(String[] args) {
